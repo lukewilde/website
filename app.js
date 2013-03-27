703b2c390b50c03b56cad1a9a11dac16f62c7899
Luke Wilde <luke.wilde@clock.co.uk>
2013-03-27 23:35:46 +0000

    Setting cache headers.

--- app.js
+++ app.js
@@ -5,4 +5,8 @@ var connect = require('connect')
 var app = connect()
   .use(connect.logger('dev'))
   .use(gzippo.staticGzip(__dirname + '/public'))
+  .use(function(req, res, next) {
+    res.set('Cache-Control', 'max-age=31536000')
+    return next()
+  })
  .listen(3111)
