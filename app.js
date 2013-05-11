70b962fab3c8c29ba1c834ef458c0c2823e1cbfe
Luke Wilde <luke.wilde@clock.co.uk>
2013-05-11 13:46:11 +0100

    Adding jade, stylus and nib.

--- app.js
+++ app.js
@@ -1,12 +1,36 @@
 var connect = require('connect')
   , gzippo = require('gzippo')
+  , express = require('express')
+  , stylus = require('stylus')
+  , nib = require('nib')
+  , app = express()
 
+function compile(str, path) {
+  return stylus(str)
+    .set('filename', path)
+    .use(nib())
+}
 
-var app = connect()
-  .use(connect.logger('dev'))
-  .use(gzippo.staticGzip(__dirname + '/public'))
-  .use(function(req, res, next) {
+app.set('views', __dirname + '/views')
+app.set('view engine', 'jade')
+app.use(express.logger('dev'))
+app.use(express.static(__dirname + '/public'))
+app.use(gzippo.staticGzip(__dirname + '/public'))
+app.use(function(req, res, next) {
     res.set('Cache-Control', 'max-age=31536000')
     return next()
   })
- .listen(3111)
+
+app.use(stylus.middleware(
+  { src: __dirname + '/public/'
+  , compile: compile
+  }
+))
+
+app.get('/test', function (req, res) {
+  res.render('test',
+  { title : 'Home' }
+  )
+})
+
+app.listen(3111)
