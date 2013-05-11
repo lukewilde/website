8ec6f99871341e32e3e7ffc97d83d151b29c8cbb
Luke Wilde <luke.wilde@clock.co.uk>
2013-05-11 15:02:17 +0100

    Tweaking stylus.

--- app.js
+++ app.js
@@ -10,25 +10,19 @@ function compile(str, path) {
     .set('filename', path)
     .use(nib())
 }
-
 app.set('views', __dirname + '/views')
 app.set('view engine', 'jade')
 app.use(express.logger('dev'))
-app.use(express.static(__dirname + '/public'))
-app.use(gzippo.staticGzip(__dirname + '/public'))
-app.use(function(req, res, next) {
-    res.set('Cache-Control', 'max-age=31536000')
-    return next()
-  })
-
 app.use(stylus.middleware(
   { src: __dirname + '/public/'
   , compile: compile
   }
 ))
 
-app.get('*', function (req, res) {
-  res.render(req.path.substring(1, req.path.length))
+app.use(express.static(__dirname + '/public'))
+
+app.get('/', function (req, res) {
+  res.render('index')
 })
 
 app.listen(3111)
