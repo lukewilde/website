cc1026e9968a53fbed39cbdfbed4d785a06d4336
Luke Wilde <luke.wilde@clock.co.uk>
2013-05-12 21:00:51 +0100

    Adding columns and dodgy responsiveness.

--- app.js
+++ app.js
@@ -27,27 +27,35 @@ app.get('/gallery', function (req, res) {
   res.render('gallery',
     { images:
       [ { name: 'Test'
+        , slug: 'test1'
+        , src: 'test.jpg'
+        }
+      , { name: 'Long Test'
+        , slug: 'test2'
+        , src: 'long-test.png'
+        }
+      , { name: 'Long Test'
+        , slug: 'test3'
+        , src: 'long-test.png'
+        }
+      , { name: 'Test'
+        , slug: 'test4'
         , src: 'test.jpg'
         }
       , { name: 'Test'
+        , slug: 'test5'
         , src: 'test.jpg'
         }
       , { name: 'Test'
+        , slug: 'test6'
         , src: 'test.jpg'
         }
-      , { name: 'Test'
-        , src: 'test.jpg'
+      , { name: 'Long Test'
+        , slug: 'test7'
+        , src: 'long-test.png'
         }
       , { name: 'Test'
-        , src: 'test.jpg'
-        }
-      , { name: 'Test'
-        , src: 'test.jpg'
-        }
-      , { name: 'Test'
-        , src: 'test.jpg'
-        }
-      , { name: 'Test'
+        , slug: 'test8'
         , src: 'test.jpg'
         }
       ]
