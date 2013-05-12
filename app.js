391e52e88280f1ecb03203d04bda3a59bff8c537
Luke Wilde <luke.wilde@clock.co.uk>
2013-05-12 21:36:16 +0100

    Multi section.

--- app.js
+++ app.js
@@ -25,74 +25,150 @@ app.use(express.static(__dirname + '/public'))
 app.get('/gallery', function (req, res) {
 
   res.render('gallery',
-    { images:
-      [ { name: 'Test'
-        , slug: 'test1'
-        , src: 'test.jpg'
+    { sections:
+      [ { name: 'portrait'
+        , images:
+          [ { name: 'Test'
+            , slug: 'test1'
+            , src: 'test.jpg'
+            }
+          , { name: 'Long Test'
+            , slug: 'test2'
+            , src: 'long-test.png'
+            }
+          , { name: 'Long Test'
+            , slug: 'test3'
+            , src: 'long-test.png'
+            }
+          , { name: 'Test'
+            , slug: 'test4'
+            , src: 'test.jpg'
+            }
+          , { name: 'Test'
+            , slug: 'test5'
+            , src: 'test.jpg'
+            }
+          , { name: 'Test'
+            , slug: 'test6'
+            , src: 'test.jpg'
+            }
+          , { name: 'Long Test'
+            , slug: 'test7'
+            , src: 'long-test.png'
+            }
+          , { name: 'Test'
+            , slug: 'test8'
+            , src: 'test.jpg'
+            }
+          , { name: 'Test'
+            , slug: 'test8'
+            , src: 'test.jpg'
+            }
+          , { name: 'Test'
+            , slug: 'test8'
+            , src: 'test.jpg'
+            }
+          , { name: 'Test'
+            , slug: 'test8'
+            , src: 'test.jpg'
+            }
+          , { name: 'Test'
+            , slug: 'test8'
+            , src: 'test.jpg'
+            }
+          , { name: 'Test'
+            , slug: 'test8'
+            , src: 'test.jpg'
+            }
+          , { name: 'Test'
+            , slug: 'test8'
+            , src: 'test.jpg'
+            }
+          , { name: 'Test'
+            , slug: 'test8'
+            , src: 'test.jpg'
+            }
+          , { name: 'Test'
+            , slug: 'test8'
+            , src: 'test.jpg'
+            }
+          , { name: 'Test'
+            , slug: 'test8'
+            , src: 'test.jpg'
+            }
+          ]
         }
-      , { name: 'Long Test'
-        , slug: 'test2'
-        , src: 'long-test.png'
-        }
-      , { name: 'Long Test'
-        , slug: 'test3'
-        , src: 'long-test.png'
-        }
-      , { name: 'Test'
-        , slug: 'test4'
-        , src: 'test.jpg'
-        }
-      , { name: 'Test'
-        , slug: 'test5'
-        , src: 'test.jpg'
-        }
-      , { name: 'Test'
-        , slug: 'test6'
-        , src: 'test.jpg'
-        }
-      , { name: 'Long Test'
-        , slug: 'test7'
-        , src: 'long-test.png'
-        }
-      , { name: 'Test'
-        , slug: 'test8'
-        , src: 'test.jpg'
-        }
-      , { name: 'Test'
-        , slug: 'test8'
-        , src: 'test.jpg'
-        }
-      , { name: 'Test'
-        , slug: 'test8'
-        , src: 'test.jpg'
-        }
-      , { name: 'Test'
-        , slug: 'test8'
-        , src: 'test.jpg'
-        }
-      , { name: 'Test'
-        , slug: 'test8'
-        , src: 'test.jpg'
-        }
-      , { name: 'Test'
-        , slug: 'test8'
-        , src: 'test.jpg'
-        }
-      , { name: 'Test'
-        , slug: 'test8'
-        , src: 'test.jpg'
-        }
-      , { name: 'Test'
-        , slug: 'test8'
-        , src: 'test.jpg'
-        }
-      , { name: 'Test'
-        , slug: 'test8'
-        , src: 'test.jpg'
-        }
-      , { name: 'Test'
-        , slug: 'test8'
-        , src: 'test.jpg'
+      , { name: 'lifestyle'
+        , images:
+          [ { name: 'Test'
+            , slug: 'test1'
+            , src: 'test.jpg'
+            }
+          , { name: 'Test'
+            , slug: 'test4'
+            , src: 'test.jpg'
+            }
+          , { name: 'Test'
+            , slug: 'test5'
+            , src: 'test.jpg'
+            }
+          , { name: 'Long Test'
+            , slug: 'test2'
+            , src: 'long-test.png'
+            }
+          , { name: 'Long Test'
+            , slug: 'test3'
+            , src: 'long-test.png'
+            }
+          , { name: 'Test'
+            , slug: 'test6'
+            , src: 'test.jpg'
+            }
+          , { name: 'Long Test'
+            , slug: 'test7'
+            , src: 'long-test.png'
+            }
+          , { name: 'Test'
+            , slug: 'test8'
+            , src: 'test.jpg'
+            }
+          , { name: 'Test'
+            , slug: 'test8'
+            , src: 'test.jpg'
+            }
+          , { name: 'Test'
+            , slug: 'test8'
+            , src: 'test.jpg'
+            }
+          , { name: 'Test'
+            , slug: 'test8'
+            , src: 'test.jpg'
+            }
+          , { name: 'Test'
+            , slug: 'test8'
+            , src: 'test.jpg'
+            }
+          , { name: 'Test'
+            , slug: 'test8'
+            , src: 'test.jpg'
+            }
+          , { name: 'Test'
+            , slug: 'test8'
+            , src: 'test.jpg'
+            }
+          , { name: 'Test'
+            , slug: 'test8'
+            , src: 'test.jpg'
+            }
+          , { name: 'Test'
+            , slug: 'test8'
+            , src: 'test.jpg'
+            }
+          , { name: 'Test'
+            , slug: 'test8'
+            , src: 'test.jpg'
+            }
+          ]
         }
       ]
     }
