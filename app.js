7daabc864c9317c544db582e3f2978749293fca8
Luke Wilde <luke.wilde@clock.co.uk>
2013-05-14 21:35:43 +0100

    Moving images into config files.

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ var connect = require('connect')
   , stylus = require('stylus')
   , nib = require('nib')
   , app = express()
+  , portraitImages = require('./views/images/portrait')
+  , lifestyleImages =require('./views/images/lifestyle')
 
 function compile(str, path) {
   return stylus(str)
@@ -27,148 +29,10 @@ app.get('/gallery', function (req, res) {
   res.render('gallery',
     { sections:
       [ { name: 'portrait'
-        , images:
-          [ { name: 'Test'
-            , slug: 'test1'
-            , src: 'test.jpg'
-            }
-          , { name: 'Long Test'
-            , slug: 'test2'
-            , src: 'long-test.png'
-            }
-          , { name: 'Long Test'
-            , slug: 'test3'
-            , src: 'long-test.png'
-            }
-          , { name: 'Test'
-            , slug: 'test4'
-            , src: 'test.jpg'
-            }
-          , { name: 'Test'
-            , slug: 'test5'
-            , src: 'test.jpg'
-            }
-          , { name: 'Test'
-            , slug: 'test6'
-            , src: 'test.jpg'
-            }
-          , { name: 'Long Test'
-            , slug: 'test7'
-            , src: 'long-test.png'
-            }
-          , { name: 'Test'
-            , slug: 'test8'
-            , src: 'test.jpg'
-            }
-          , { name: 'Test'
-            , slug: 'test8'
-            , src: 'test.jpg'
-            }
-          , { name: 'Test'
-            , slug: 'test8'
-            , src: 'test.jpg'
-            }
-          , { name: 'Test'
-            , slug: 'test8'
-            , src: 'test.jpg'
-            }
-          , { name: 'Test'
-            , slug: 'test8'
-            , src: 'test.jpg'
-            }
-          , { name: 'Test'
-            , slug: 'test8'
-            , src: 'test.jpg'
-            }
-          , { name: 'Test'
-            , slug: 'test8'
-            , src: 'test.jpg'
-            }
-          , { name: 'Test'
-            , slug: 'test8'
-            , src: 'test.jpg'
-            }
-          , { name: 'Test'
-            , slug: 'test8'
-            , src: 'test.jpg'
-            }
-          , { name: 'Test'
-            , slug: 'test8'
-            , src: 'test.jpg'
-            }
-          ]
+        , images: portraitImages
         }
       , { name: 'lifestyle'
-        , images:
-          [ { name: 'Test'
-            , slug: 'test1'
-            , src: 'test.jpg'
-            }
-          , { name: 'Test'
-            , slug: 'test4'
-            , src: 'test.jpg'
-            }
-          , { name: 'Test'
-            , slug: 'test5'
-            , src: 'test.jpg'
-            }
-          , { name: 'Long Test'
-            , slug: 'test2'
-            , src: 'long-test.png'
-            }
-          , { name: 'Long Test'
-            , slug: 'test3'
-            , src: 'long-test.png'
-            }
-          , { name: 'Test'
-            , slug: 'test6'
-            , src: 'test.jpg'
-            }
-          , { name: 'Long Test'
-            , slug: 'test7'
-            , src: 'long-test.png'
-            }
-          , { name: 'Test'
-            , slug: 'test8'
-            , src: 'test.jpg'
-            }
-          , { name: 'Test'
-            , slug: 'test8'
-            , src: 'test.jpg'
-            }
-          , { name: 'Test'
-            , slug: 'test8'
-            , src: 'test.jpg'
-            }
-          , { name: 'Test'
-            , slug: 'test8'
-            , src: 'test.jpg'
-            }
-          , { name: 'Test'
-            , slug: 'test8'
-            , src: 'test.jpg'
-            }
-          , { name: 'Test'
-            , slug: 'test8'
-            , src: 'test.jpg'
-            }
-          , { name: 'Test'
-            , slug: 'test8'
-            , src: 'test.jpg'
-            }
-          , { name: 'Test'
-            , slug: 'test8'
-            , src: 'test.jpg'
-            }
-          , { name: 'Test'
-            , slug: 'test8'
-            , src: 'test.jpg'
-            }
-          , { name: 'Test'
-            , slug: 'test8'
-            , src: 'test.jpg'
-            }
-          ]
+        , images: lifestyleImages
         }
       ]
     }
