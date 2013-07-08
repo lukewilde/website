3a03db6f89c3ae308d3aff9cc3896030a1c0e872
Luke Wilde <luke.wilde@clock.co.uk>
2013-07-08 23:18:31 +0100

    Rending images on gallery page.

--- routes.js
+++ routes.js
@@ -9,8 +9,12 @@ function makeRoutes(app) {
     indexView(req, res)
   })
 
-  app.get('/gallery', function (req, res) {
-    galleryView(req, res)
+  app.get('/gallery(/:type)', function (req, res) {
+    return galleryView(req, res)
+  })
+
+  app.get('/gallerys', function (req, res) {
+    return galleryView(req, res)
   })
 
 }
--- views/gallery.js
+++ views/gallery.js
@@ -1,16 +1,25 @@
-var lifestyleImages = require('./images/lifestyle')
-  , portraitImages = require('./images/portrait')
+var _ = require('lodash')
+  , getImage = require('../lib/get-image')()
 
 module.exports = function(req, res) {
-  res.render('gallery',
-     { sections:
-       [ { name: 'portrait'
-         , images: portraitImages
-         }
-       , { name: 'lifestyle'
-         , images: lifestyleImages
-         }
-       ]
-     }
-   )
+
+  var reqHasNoParams = req.params.length <= 0
+    , isInvalidType = !_.has(getImage.types, req.params)
+    , type = req.params[0]
+
+  if (reqHasNoParams || isInvalidType) {
+    type = 'portraits'
+  }
+
+  getImage.getCollection(type, 'small', function(err, images) {
+
+    if (err) {
+      console.log(err)
+      res.render('500')
+    }
+
+    res.render('gallery',
+       { images: images }
+     )
+  })
 }
