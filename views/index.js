15e1f311799d3f0e780fcc1f7b57ace7ad862874
Luke Wilde <luke.wilde@clock.co.uk>
2013-07-08 21:31:16 +0100

    Adding dodgy 500 page.

--- views/index.js
+++ views/index.js
@@ -3,6 +3,11 @@ var getImage = require('../lib/get-image')()
 module.exports = function(req, res) {
 
   getImage.getRandom('big', function(image) {
+
+    if (err) {
+      res.render('500')
+    }
+
     res.render('index', { image: image })
   })
 }
