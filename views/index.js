298220b3b04b0af2392a634ac1015a3812fa31c3
Luke Wilde <luke.wilde@clock.co.uk>
2013-07-09 22:34:01 +0100

    Getting correct image size.

--- views/index.js
+++ views/index.js
@@ -2,7 +2,7 @@ var getImage = require('../lib/get-image')()
 
 module.exports = function(req, res) {
 
-  getImage.getRandom('small', function(err, image) {
+  getImage.getRandom('big', function(err, image) {
 
     if (err) {
       res.render('500')
