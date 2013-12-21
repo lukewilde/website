3c992f3f8e3f7d983caec8129929fdd0ea286ae3
Luke Wilde <luke.wilde@clock.co.uk>
2013-12-21 19:45:10 +0000

    Adding next and previous url's

--- views/gallery-item.js
+++ views/gallery-item.js
@@ -10,10 +10,22 @@ module.exports = function(req, res, imageCache, stock) {
     return
   }
 
-  image = _.find(imageCache[req.params.type], { name: image })
+  var imageByType = imageCache[req.params.type]
+    , imageIndex = _.findIndex(imageByType, { name: image })
+    , next = imageByType[imageIndex +1]
+    , image = imageByType[imageIndex]
+    , prev = imageByType[imageIndex -1]
+
+  next = next ? next : false
+  prev = prev ? prev : false
 
   if (image) {
-    res.render('gallery-item', { image: image, imageCache: imageCache })
+    res.render('gallery-item',
+      { image: image
+      , next: next
+      , prev: prev
+      }
+    )
   } else {
     res.render('404')
   }
