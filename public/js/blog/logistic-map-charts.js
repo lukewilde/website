1e9e2037e0d51788e8aa4cf543ce145f4fd348c1
Luke Wilde <luke.wilde@clock.co.uk>
2014-05-22 00:39:37 +0100

    lint.

--- public/js/blog/logistic-map-charts.js
+++ public/js/blog/logistic-map-charts.js
@@ -1,47 +1,40 @@
-var margin = {top: 20, right: 20, bottom: 30, left: 50},
-    width = 960 - margin.left - margin.right,
-    height = 500 - margin.top - margin.bottom;
+var margin =
+    { top: 20
+    , right: 20
+    , bottom: 30
+    , left: 50
+    }
+  , width = 960 - margin.left - margin.right
+  , height = 500 - margin.top - margin.bottom
+  , parseDate = window.d3.time.format('%d-%b-%y').parse
+  , x = window.d3.time.scale().range([0, width])
+  , y = window.d3.scale.linear().range([height, 0])
+  , xAxis = window.d3.svg.axis().scale(x).orient('bottom')
+  , yAxis = window.d3.svg.axis().scale(y).orient('left')
 
-var parseDate = d3.time.format('%d-%b-%y').parse;
+var line = window.d3.svg.line()
+    .x(function(d) { return x(d.date) })
+    .y(function(d) { return y(d.close) })
 
-var x = d3.time.scale()
-    .range([0, width]);
-
-var y = d3.scale.linear()
-    .range([height, 0]);
-
-var xAxis = d3.svg.axis()
-    .scale(x)
-    .orient('bottom');
-
-var yAxis = d3.svg.axis()
-    .scale(y)
-    .orient('left');
-
-var line = d3.svg.line()
-    .x(function(d) { return x(d.date); })
-    .y(function(d) { return y(d.close); });
-
-var svg = d3.selectAll('.graph-01').append('svg')
+var svg = window.d3.selectAll('.graph-01').append('svg')
     .attr('width', width + margin.left + margin.right)
     .attr('height', height + margin.top + margin.bottom)
-  .append('g')
-    .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
+    .append('g')
+    .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')')
 
-d3.tsv('/js/blog/data.tsv', function(error, data) {
-  console.log(error, data)
+window.d3.tsv('/js/blog/data.tsv', function(error, data) {
   data.forEach(function(d) {
-    d.date = parseDate(d.date);
-    d.close = +d.close;
-  });
+    d.date = parseDate(d.date)
+    d.close = +d.close
+  })
 
-  x.domain(d3.extent(data, function(d) { return d.date; }));
-  y.domain(d3.extent(data, function(d) { return d.close; }));
+  x.domain(window.d3.extent(data, function(d) { return d.date }))
+  y.domain(window.d3.extent(data, function(d) { return d.close }))
 
   svg.append('g')
       .attr('class', 'x axis')
       .attr('transform', 'translate(0,' + height + ')')
-      .call(xAxis);
+      .call(xAxis)
 
   svg.append('g')
       .attr('class', 'y axis')
@@ -51,10 +44,10 @@ d3.tsv('/js/blog/data.tsv', function(error, data) {
       .attr('y', 6)
       .attr('dy', '.71em')
       .style('text-anchor', 'end')
-      .text('Price ($)');
+      .text('Price ($)')
 
   svg.append('path')
       .datum(data)
       .attr('class', 'line')
-      .attr('d', line);
-});
+      .attr('d', line)
+})
