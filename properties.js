8dc6ea1218ae858de3d27017289059d199b4a758
Luke Wilde <luke.wilde@gmail.com>
2017-02-02 19:29:46 +0000

    Adjusting page title.s

--- properties.js
+++ properties.js
@@ -1,5 +1,5 @@
 module.exports =
-  { title: 'Luke Wilde | Freelance web and games programmer'
+  { title: 'Luke Wilde | Freelance Web and Games Programmer'
   , description: 'A blog from a JavaScript web and games developer.'
   , siteUrl: 'http://lukewilde.co.uk'
   }
